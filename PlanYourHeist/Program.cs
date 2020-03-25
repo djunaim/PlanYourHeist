0f2e6d2ffc5bcc26d680b0abb526fc78753a96fa
Monica Djunaidi <modjun12@gmail.com>
2020-03-25 18:40:36 -0500

    complete

--- PlanYourHeist/Program.cs
+++ PlanYourHeist/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PlanYourHeist
 {
@@ -8,18 +10,38 @@ namespace PlanYourHeist
         {
             Console.WriteLine("Plan Your Heist!");
 
-            var teamMember1 = new TeamMember();
+            List<TeamMember> teamMembers = new List<TeamMember>();
 
-            Console.WriteLine("Please enter a team member's first name.");
-            teamMember1.Name = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("Please enter a team member's first name.");
+                var teamMemberName = Console.ReadLine();
+                if (teamMemberName == "") break;
 
-            Console.WriteLine("Please enter team member's skill level between 0 to 10.");
-            teamMember1.SkillLevel = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter team member's skill level between 0 to 10.");
+                var teamMemberSkillLevel = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Please enter team member's courage factor between 0.0 to 2.0.");
-            teamMember1.CourageFactor = decimal.Parse(Console.ReadLine());
+                Console.WriteLine("Please enter team member's courage factor between 0.0 to 2.0.");
+                var teamMemberCourageFactor = decimal.Parse(Console.ReadLine());
 
-            Console.WriteLine($"{teamMember1.Name} has a skill level of {teamMember1.SkillLevel} and courage factor of {teamMember1.CourageFactor}");
+                var teamMember = new TeamMember
+                {
+                    Name = teamMemberName,
+                    SkillLevel = teamMemberSkillLevel,
+                    CourageFactor = teamMemberCourageFactor
+                };
+
+                teamMembers.Add(teamMember);
+
+            }
+
+            var numOfMembers = teamMembers.Count();
+            Console.WriteLine($"there are {numOfMembers} members");
+
+            foreach (var teamMember in teamMembers)
+            {
+                Console.WriteLine($"{teamMember.Name} has a skill level of {teamMember.SkillLevel} and courage factor of {teamMember.CourageFactor}");
+            }
         }
     }
 }
